76e32b6858cffbf05ea62e4705f06d781af2c673
D.J. Crosby <taladan@gmail.com>
2023-04-03 16:07:20 -0400

    Refactor game loop and variables
    Move around some of the chunks of logic into single
    purpose methods.  Implement a very basic win state.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,38 +1,59 @@
 require_relative "guess"
 require_relative "hm_graphics"
 require_relative "mask"
+require "pry-byebug"
 
 class Hangman
-  # Remove me.
-  # attr_accessor :word
   include Graphics
   def initialize()
     @grph = Graphics.ascii
-    @turns = @grph.length - 1
+    @turns = @grph.length
     @wrong_guesses = 0
     @word = get_word()
     @header = "Now playing Hangman!"
     @prompt = "Please choose a letter: "
+    @mask = Mask.new(@word)
+    @guess = Guess.new(@word)
   end
 
   def start()
-    game_loop(Mask.new(@word), Guess.new(@word))
+    game_loop()
   end
 
-  def game_loop(mask, guesses)
-    while @wrong_guesses < @turns
-      display(mask.secret)
-      result = guesses.check(prompt(mask.secret))
-      if result != nil
-        guess, guess_opts = result.first
-        @wrong_guesses += 1 unless guess_opts[:is_correct]
-        if guess_opts[:is_correct]
-          mask.replace_index(guess_opts[:indices_to_replace], guess)
-        end
+  private
+
+  def process_guess(result)
+    if result != nil
+      player_guess, player_guess_opts = result.first
+      @wrong_guesses += 1 unless player_guess_opts[:is_correct]
+      if player_guess_opts[:is_correct]
+        @mask.replace_index(
+          player_guess_opts[:indices_to_replace],
+          player_guess,
+        )
       else
-        puts center("You've already guessed that letter.")
-        sleep(2)
+        puts center("There are no #{player_guess.upcase}'s in the word.")
+        sleep(1)
       end
+    else
+      puts center("You've already guessed that letter.")
+      sleep(1)
+    end
+  end
+
+  def game_loop()
+    win_state = false
+    while @wrong_guesses < @turns && !win_state
+      win_state = true if @mask.secret == @mask.word
+      if win_state
+        @mask.secret = @mask.word
+        display(@mask.secret, nil)
+        puts center("You win!")
+        break
+      end
+      display(@mask.secret, @guess.already_guessed)
+      process_guess(@guess.check(prompt(@mask.secret)))
+      puts center("The word was: #{@mask.word}")
     end
   end
 
@@ -46,8 +67,7 @@ class Hangman
     guess
   end
 
-  def display(secret)
-    system("clear") || system("cls")
+  def update_screen(guesses)
     # puts Graphics.pad(@header.length) + @header
     line_pad(3)
     puts center(@header)
@@ -57,8 +77,15 @@ class Hangman
     # puts Graphics.pad(@instructions.length) + @instructions
     puts center("You have #{@turns - @wrong_guesses} guesses left.")
     line_pad(2)
-    puts center(secret.split("").join(" "))
-    line_pad(3)
+    puts center(@mask.secret.split("").join(" "))
+    line_pad(1)
+    puts center("Letters tried: #{guesses.keys}") if guesses
+    line_pad(1)
+  end
+
+  def display(secret, guesses)
+    system("clear") || system("cls")
+    update_screen(guesses)
   end
 
   def center(text)
@@ -72,9 +99,6 @@ class Hangman
 
   def line_pad(int)
     puts "\n" * int
-  end
-
-  def check_guess(guess)
   end
 
   def save()
@@ -93,6 +117,6 @@ class Hangman
         word_array.push(line) if line.length.between?(lower, upper)
       end
     word = word_array.sample()
-    word
+    word.chomp
   end
 end
