f374dddd065220ff87b4f49fe785313867cb1239
D.J. Crosby <taladan@gmail.com>
2023-04-04 09:31:21 -0400

    Refactor for replayability

--- main.rb
+++ main.rb
@@ -1,8 +1,51 @@
 require "./lib/hangman.rb"
+require "./lib/display"
+require "pry-byebug"
+
+def play_or_load(display)
+  output = ""
+  until output =~ /^[plPL]$/
+    system("clear") || system("cls")
+    display.screen_center(
+      "Would you like to [P]lay a new game or [L]oad a saved game?",
+    )
+    output = gets.chomp
+  end
+  output
+end
+
+def save()
+end
+
+def load()
+end
+
+def play_again?()
+  output = ""
+  until output.downcase =~ /^[yn]$/
+    puts "Play again (y/n)? "
+    output = gets.chomp until output.downcase =~ /^[yn]$/
+  end
+  output == "y"
+end
 
 def main()
-  game = Hangman.new()
-  game.start
+  replay = true
+  while replay
+    start()
+    replay = play_again?
+  end
+end
+
+def start()
+  display = Display.new()
+  response = play_or_load(display)
+  game_data = Hash.new(nil)
+  if response == "l"
+    game_data = load()
+  else
+    game = Hangman.new(display, game_data)
+  end
 end
 
 main()
